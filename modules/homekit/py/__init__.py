6345fffa2561c3063d1fee98fdc2d0969303785e
Wilko Eisele <wilkobehrend@mac.com>
2022-01-10 21:08:05 +0100

    Fix crash & add bridged accessory
    - change the callback mechanism (like in pycameresp)
    - added functions to add brdiged accessory

--- modules/homekit/py/__init__.py
+++ modules/homekit/py/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2021 Remi BERTHOLET
 """ Class to manage homekit """
 from homekit.homekit   import *
-from homekit.service   import *
+from homekit.server    import *
 from homekit.charact   import *
 from homekit.accessory import *
